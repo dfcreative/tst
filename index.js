461b1dfe9815f0871bc5dfde01d0e7f3fe22569f
Dmitry Yv <df.creative@gmail.com>
2019-10-15 16:34:14 -0400

    Outline basice

--- index.js
+++ index.js
@@ -1,136 +1,96 @@
-let assert = require('./assert.js')
+import fastDeepEqual from 'fast-deep-equal'
 
 const isNode = typeof process !== 'undefined' && Object.prototype.toString.call(process) === '[object process]'
 
-function Test(o) {
-  Object.assign(this, o)
-  this.assertion = []
-}
-Test.prototype.run = async function run () {
-  await this.fn(this)
-}
-Object.assign(Test.prototype, {
-  skip: false,
-  todo: false,
-  only: false,
-  fn: null
-}, assert)
 
-export default function test (name, fn) {
-  if (!fn) return test.todo(name)
-  let t = new Test({ name, fn })
-  tests.push(t)
-  start()
-  return t
-}
-test.todo = function (name, fn) {
-  let t = new Test({ name, fn, todo: true })
-  tests.push(t)
-  start()
-  return t
-}
-test.skip = function (name, fn) {
-  let t = new Test({ name, fn, skip: true })
-  tests.push(t)
-  start()
-  return t
-}
-test.only = function (name, fn) {
-  let t = new Test({ name, fn, only: true })
-  tests.push(t)
-  start()
-  return t
-}
-
-let assertIndex = 0
-
-const tests = []
+let only = 0
+export let current = null
 let passed = 0
 let failed = 0
 let skipped = 0
+let running = false
+let planned = new Set()
 
-export let current = null // current test
 
-export function log (ok, operator, msg, info = {}) {
-  assertIndex += 1
-  if (ok) {
-    current.assertion.push({ idx: assertIndex, msg })
-    isNode ?
-    console.log(`✔ ${ assertIndex } — ${ msg }`) :
-    console.log(`%c✔ ${assertIndex} — ${msg}`, 'color: #229944')
-    passed += 1
-  } else {
-    current.assertion.push({ idx: assertIndex, msg, info, error: new Error() })
-    failed += 1
-    console.assert(false, `${assertIndex} — ${msg}`, info, (new Error()))
-  }
+export function test(name, fn) {
+  if (!fn) return test.todo(name)
+  fn._name = name
+  planned.add(fn)
+  start()
 }
-
-
-let ondone, only = 0, running = false
+test.todo = function (name, fn) {
+  skipped++
+  fn._name = name
+  fn._todo = true
+  start()
+}
+test.skip = function (name, fn) {
+  skipped++
+  fn._name = name
+  fn._skip = true
+  planned.add(fn)
+  start()
+}
+test.only = function (name, fn) {
+  only++
+  fn._name = name
+  fn._only = true
+  planned.add(fn)
+  start()
+}
 
 
 function start() {
-  if (!running) {
-    running = true
-
-    Promise.resolve().then(() => {
-      tests.forEach(test => test.only && only++)
-
-      dequeue()
-    })
+  if (running) return
+  running = true
+  let p = Promise.resolve()
+  for (let fn of planned) {
+    if (only && !fn._only) continue
+    p.then(() => run(fn)).then(() => passed++, () => skipped++)
   }
+
+  p.then(() => {
+    // summarise
+    console.log(`\n═════\n`)
+    const total = passed + failed + skipped
+    if (only) console.log(`# total ${only} of ${total}`)
+    else console.log(`# total ${total}`)
+    console.log(`✔ passed ${passed}`)
+    console.log(`✘ failed ${failed}`)
+    if (skipped) console.log(`⋯ skipped ${skipped}`)
+  })
 }
 
-async function dequeue () {
-  if (tests.length) {
-    const test = tests.shift()
-
-    if (only && !test.only) {
-      // in only-run - ignore tests
-      skipped += 1
-      return dequeue()
-    }
-
-    if (test.skip) {
-      isNode ?
-      console.log(`# skip ${test.name}`) :
-      console.log(`%c↪️ skip ${test.name}`, 'color: #ddd')
-      skipped += 1
-      return dequeue()
-    }
-    if (test.todo) {
-      console.log(`🚧 ${test.name}`)
-      return dequeue()
-    }
-
-    try {
-      current = test
-      console.log(`# ${test.name}`)
-      await test.run()
-    } catch (err) {
-      failed += 1
-      // FIXME: this syntax is due to chrome not always able to grasp the stack trace from source maps
-      console.error(err.stack)
-    }
-
-    return dequeue()
+function run(fn) {
+  if (fn._skip) {
+    isNode ?
+      console.log(`⋯ ${fn._name}`) :
+      console.log(`%c⋯ ${fn._name}`, 'color: #ddd')
+    return
   }
 
-  // summarise
-  console.log(`---`)
-  const total = passed + failed + skipped
-  if (only) console.log(`# only ${only} cases`)
-  console.log(`# total ${ total }`)
-  if (passed) console.log(`# pass ${passed}`)
-  if (failed) console.log(`# fail ${failed}`)
-  if (skipped) console.log(`# skip ${skipped}`)
+  if (fn._todo) {
+    console.log(`🚧 ${fn._name}`)
+    return
+  }
 
-  ondone()
-  if (isNode) process.exit(failed ? 1 : 0)
+  current = fn
+  console.log(`# ${fn._name}`)
+  return (new Promise(fn)).catch(err => {
+    failed += 1
+    // FIXME: this syntax is due to chrome not always able to grasp the stack trace from source maps
+    console.error(err.stack)
+  })
 }
 
 
-export const done = new Promise((resolve) => {
-  ondone = resolve
-})
+export function is(a, b, msg = 'should be the same') {
+  if (arguments.length < 3 && typeof a === 'boolean') {
+    if (typeof b === 'string') msg = b
+    b = Boolean(b)
+  }
+
+  console.assert(fastDeepEqual(a, b), `${msg}`, { actual: a, expected: b }, new Error())
+}
+
+export default test
--- test.js
+++ test.js
@@ -1,34 +1,34 @@
-import t from '.'
+import { test, is } from '.'
 
-t('x', t => {
-  t.eq(1, 2)
-  t.neq(1, 1)
-  t.deq([1], [1,2])
-  t.pass('passed')
+test.only('basic tests', pass => {
+  is(1, 2)
+  is(1 !== 1)
+  is([1], [1,2])
+  pass('passed')
 })
 
-t('these tests will all pass', t => {
-  t.ok(true);
-  t.ok(true, 'this time with an optional message');
-  t.ok('not true, but truthy enough');
+test('these tests will all pass', pass => {
+  is(true);
+  is(true, 'this time with an optional message');
+  is('not true, but truthy enough');
 
-  t.eq(1 + 1, 2);
-  t.eq(Math.max(1, 2, 3), 3);
+  is(1 + 1, 2);
+  is(Math.max(1, 2, 3), 3);
 
-  t.err(() => {
+  err(() => {
     throw new Error('oh no!');
   }, /oh no!/);
 
-  t.pass('ok')
+  pass('ok')
 })
 
-t('these tests will not pass', t => {
-  t.eq(42, '42');
-  t.eq({}, {});
+test('these tests will not pass', t => {
+  eq(42, '42');
+  eq({}, {});
 
-  t.fail('nok')
+  fail('nok')
 })
 
-t.skip('this test will not run', t => {
-  t.pass('ok')
+test.skip('this test will not run', t => {
+  pass('ok')
 })
